cc15ad3466a989beabd1006a539614927fda291f
Paolo Stivanin <info@paolostivanin.com>
2025-10-21 11:15:51 +0200

    Update to 6.17.4 and fix scripts

--- prepare-files.sh
+++ prepare-files.sh
@@ -1,32 +1,29 @@
 #!/bin/bash
+set -euo pipefail
 
-KERNEL_VERSION="6.6"
+KERNEL_VERSION="6.17.4"
 LINK="https://www.kernel.org/pub/linux/kernel/v6.x/linux-${KERNEL_VERSION}.tar.xz"
+WORKDIR="/tmp/linux-${KERNEL_VERSION}"
 
-TBL_64="/tmp/linux-${KERNEL_VERSION}/arch/x86/entry/syscalls/syscall_64.tbl"
-
-if [ ! -d ${DIR}/linux-${KERNEL_VERSION} ];then
-    curl -L $LINK > /tmp/linux-${KERNEL_VERSION}.tar.xz
-    tar xf /tmp/linux-${KERNEL_VERSION}.tar.xz -C /tmp/
+if [ ! -d "$WORKDIR" ]; then
+    curl -L "$LINK" -o "${WORKDIR}.tar.xz"
+    tar -xf "${WORKDIR}.tar.xz" -C /tmp/
 fi
 
-if [ ! -f ${TBL_64} ];then
-	echo "File syscall_64.tbl doesn't exist"
-	exit -1
+TBL_64="${WORKDIR}/arch/x86/entry/syscalls/syscall_64.tbl"
+if [ ! -f "$TBL_64" ]; then
+    echo "File syscall_64.tbl doesn't exist"
+    exit 1
 fi
 
-echo "[+] Generating tags, this may take a while..."
-ctags --fields=afmikKlnsStz --c-kinds=+pc -R /tmp/linux-${KERNEL_VERSION}
-echo "[+] Tags generated"
-echo "[+] Preparing the syscall table file..."
-cp -v $TBL_64 .
-sed -i "s/__x64_//g" syscall_64.tbl
-sed -i "s/__x32_compat_//g" syscall_64.tbl
-echo "[+] Done :)"
-rm -rf "/tmp/linux-${KERNEL_VERSION}"
-rm -rf "/tmp/linux-${KERNEL_VERSION}.tar.xz"
-echo "[I] Calling gen_syscalls.py..."
-./gen_syscalls.py > www/syscalls-x86_64.js
-rm -rf tags
-rm -rf syscall_64.tbl
-sed -i "s/\/tmp\/linux-${KERNEL_VERSION}\///g" www/syscalls-x86_64.js
+echo "[+] Generating tags..."
+ctags --fields=+nKsSt --languages=C --output-format=json -R "$WORKDIR" > "${WORKDIR}/tags.json"
+
+cp "$TBL_64" syscall_64.tbl
+sed -i 's/__x64_//g; s/__x32_compat_//g' syscall_64.tbl
+
+echo "[+] Running gen_syscalls.py..."
+KERNEL_VERSION=$KERNEL_VERSION ./gen_syscalls.py > www/syscalls-x86_64.js
+
+rm -rf "$WORKDIR" "${WORKDIR}.tar.xz" syscall_64.tbl
+echo "[+] Done!"
